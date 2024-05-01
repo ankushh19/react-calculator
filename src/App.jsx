44f900a6c22d078b5b93a0e5378cb26fdc1ad12f
ankushh19 <ak9108824@gmail.com>
2024-05-01 18:30:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  return (
+    <div>
+      <Wrapper>
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button value={btn} key={i} />
+          ))}
+        </ButtonBox>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Screen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Screen = () => {
+  return <div className="screen">0323223</div>;
+};
+
+export default Screen;
